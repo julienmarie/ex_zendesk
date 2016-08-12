93f29df40b3ae2a6d5f4d951402063ac6e03ed93
Andrew Schmid <andrew@storestartup.com>
2016-08-12 13:16:38 -0400

    description and package
    updated for submitting to hex.pm

--- mix.exs
+++ mix.exs
@@ -5,28 +5,31 @@ defmodule ExZendesk.Mixfile do
     [app: :ex_zendesk,
      version: "0.1.0",
      elixir: "~> 1.3",
+     description: description(),
+     pacakge: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison, :poison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.9.0"}, {:poison, "~> 2.0"}]
+  end
+
+  defp description do
+    """
+    A simple HTTPoison wrapper for interacting with the Zendesk API
+    """
+  end
+
+  defp package do
+    [name: :ex_zendesk,
+     maintainers: ["Andrew Schmid"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/storestartup/ex_zendesk"}]
   end
 end
